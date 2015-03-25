64c7d6dc4c4278af0f8344f6aacf952b609b851e
Justin Farooq <justinfarooq@gmail.com>
2015-03-24 20:43:06 -0700

    scrollzer working

--- js/active.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	$('#nav a').on('click', function() {
-		var lastActive = $('#nav .active');
-		var $li = $(event.target).closest('li');
-		$li.addClass('active');
-		lastActive.removeClass('active');
-	});
-}).call(this);
--- js/init.js
+++ js/init.js
@@ -96,6 +96,48 @@
 		// Scrolly links.
 			$('.scrolly').scrolly(1000, -10);
 
+			// Nav.
+			var $nav_a = $('#nav a');
+
+			// Scrolly-fy links.
+				$nav_a
+					.scrolly()
+					.on('click', function(e) {
+
+						var t = $(this),
+							href = t.attr('href');
+
+						if (href[0] != '#')
+							return;
+
+						e.preventDefault();
+
+						// Clear active and lock scrollzer until scrolling has stopped
+							$nav_a
+								.removeClass('active')
+								.addClass('scrollzer-locked');
+
+						// Set this link to active
+							t.addClass('active');
+
+					});
+
+			// Initialize scrollzer.
+				var ids = [];
+
+				$nav_a.each(function() {
+
+					var href = $(this).attr('href');
+
+					if (href[0] != '#')
+						return;
+
+					ids.push(href.substring(1));
+
+				});
+
+				$.scrollzer(ids, { pad: 200, lastHack: true });
+
 		// Header.
 		// If the header is using "alt" styling and #banner is present, use scrollwatch
 		// to revert it back to normal styling once the user scrolls past the banner.
